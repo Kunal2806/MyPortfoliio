33e17f3efd771ecfe9c63c2d928a8e4df8bc970f
unknown <kunalgoswami.2806@gmail.com>
2025-01-13 02:15:31 +0530

    home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,7 +3,21 @@ import React from 'react'
 function Home() {
   return (
     <div className='ContentContainer' id='Home' >
-      Home
+      <div className="homeIntro">
+        <div className="homeIntroDetails">
+          <p id='t1'>Hii, I am</p>
+          <p id='t2'> Kunal Goswami</p>
+          <p id='t3'>FUllStack WebDeveloper</p>
+        </div>
+        <div className="homeIntroImage">
+          <img src="./image/LinkedIn.png" alt="LinkedIn" />
+          <img src="./image/LeetCode.png" alt="LeetCode" />
+          <img src="./image/Figma.png" alt="Figma" />
+          <img src="./image/GitHub.png" alt="GitHub" />
+          <img src="./image/resume.png" alt="Resume" />
+        </div>
+      </div>
+      <div className="homePic"></div>
     </div>
   )
 }
