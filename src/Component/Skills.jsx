7150cdc168f404a079feeaf754356408475e2de8
unknown <kunalgoswami.2806@gmail.com>
2025-01-14 01:31:07 +0530

    skills

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -3,7 +3,74 @@ import React from 'react'
 const Skills = () => {
   return (
     <div className='ContentContainer' id='Skills'>
-      Skills
+      <div className="skillContainer">
+        <div className="column">
+        <div className='skills'>
+          <img src="./image/C.png" alt="C" />
+          <p>C</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/C++.png" alt="C" />
+          <p>C++</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/java.png" alt="C" />
+          <p>Java</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/HTML.png" alt="C" />
+          <p>HTML5</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/CSS.png" alt="C" />
+          <p>CSS3</p>
+        </div>
+        </div>
+        <div className="column">
+        <div className='skills'>
+          <img src="./image/Bootstrap.png" alt="C" />
+          <p>BootStrap</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/Javascript.png" alt="C" />
+          <p>JavaScript</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/React.png" alt="C" />
+          <p>React</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/Hono.png" alt="C" />
+          <p>HonoJs</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/DSA.png" alt="C" />
+          <p>DSA</p>
+        </div>
+        </div>
+        <div className="column">
+        <div className='skills'>
+          <img src="./image/Git.png" alt="C" />
+          <p>Git</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/GitHub.png" alt="C" />
+          <p>GitHub</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/Figma.png" alt="C" />
+          <p>Figma</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/MySql.png" alt="C" />
+          <p>MySql</p>
+        </div>
+        <div className='skills'>
+          <img src="./image/Vscode.png" alt="C" />
+          <p>VsCode</p>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
