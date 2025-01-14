aa22c812c14e7feb9054d45648931f2d18c90a77
unknown <kunalgoswami.2806@gmail.com>
2025-01-15 00:10:36 +0530

    add links

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -10,11 +10,11 @@ function Home() {
           <p id='t3'>FUllStack WebDeveloper</p>
         </div>
         <div className="homeIntroImage">
-          <img src="./image/LinkedIn.png" alt="LinkedIn" />
-          <img src="./image/LeetCode.png" alt="LeetCode" />
-          <img src="./image/Figma.png" alt="Figma" />
-          <img src="./image/GitHub.png" alt="GitHub" />
-          <img src="./image/resume.png" alt="Resume" />
+          <a href="#" target='_blank'><img src="./image/LinkedIn.png" alt="LinkedIn"/></a>
+          <a href="https://leetcode.com/u/Kunal__2806" target='_blank'><img src="./image/LeetCode.png" alt="LeetCode" /></a>
+          <a href="https://www.figma.com/files/team/1420722685421903072/recents-and-sharing?fuid=1420722683193932299" target='_blank'><img src="./image/Figma.png" alt="Figma" /></a>
+          <a href="https://github.com/Kunal2806" target='_blank'><img src="./image/GitHub.png" alt="GitHub" /></a>
+          <a href="#"><img src="./image/resume.png" alt="Resume" /></a>
         </div>
       </div>
       <div className="homePic">
