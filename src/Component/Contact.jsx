5c43868b66a8d4cb8a60631647132f5019a657d5
unknown <kunalgoswami.2806@gmail.com>
2025-01-14 23:46:37 +0530

    all done

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Contact = () => {
+
+const [detail, setdetail] = useState({name:"",email:"",phone:"",message:""});
+
+const handleChange = (e) =>{
+  const {name , value} = e.target;
+  setdetail( (pre) => ({...pre,[name]:value}));
+}
+const handleSubmit = async() =>{
+  let data = detail;
+  await fetch("http://127.0.0.1:8787/post", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type" : "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  setdetail({name:'',email:'',phone:'',message:''})
+}
+
   return (
-    <div  className='ContentContainer' id='Contact'></div>
+    <div  className='ContentContainer' id='Contact'>
+      <img src="./image/working.png" alt="pic" />
+      <div className="contactDetails">
+        <input type='text' value={detail.name} name="name" id="name" placeholder='🖊_Name' onChange={handleChange}></input>
+        <input type='email' value={detail.email} name="email" id="email" placeholder='✉_Email' onChange={handleChange}></input>
+        <input type='number' value={detail.phone} name="phone" id="phone" placeholder='☎_Phone' onChange={handleChange}></input>
+        <input type='text' value={detail.message} name="message" id="message" placeholder='🌹_Message' onChange={handleChange}></input>
+        <button type='submit' onClick={handleSubmit}>Send</button>
+      </div>
+    </div>
   )
 }
 
